a1990a5b80c7bb463cef015dfe804e30cec7019a
audrie talbot & john young <student@epicodus.com>
2015-04-29 11:15:24 -0700

    pass rspec on save, all, clear methods

--- lib/car_dealership.rb
+++ lib/car_dealership.rb
@@ -1,24 +1,32 @@
 class Vehicle
-  @@vehicles = []
-  define_method(:initialize) do |make, model, year|
-    @make = make
-    @model = model
-    @year = year
+  @@vehicles = [] #Creates a class variable called '@@vehicles' which is an emtpy array
+  define_method(:initialize) do |make, model, year| #Initializes the class Vehicle passes in the parameters: make, model, year.
+    @make = make #Creates an instance variable of the parameter make
+    @model = model #Creates an instance variable of the parameter model
+    @year = year #Creates an instance variable of the paremeter year
   end
 
-  define_method(:make) do
+  define_method(:make) do #Calling the instance variable make
     @make
   end
 
-  define_method(:model) do
+  define_method(:model) do #Calling the instance variable model
     @model
   end
 
-  define_method(:year) do
+  define_method(:year) do #Calling the instance variable year
     @year
   end
-  
-  define_method(:save) do
+
+  define_method(:save) do #Pushing user input into the '@@vehicles' array
   @@vehicles.push(self)
+  end
+
+  define_singleton_method(:all) do
+    @@vehicles
+  end
+
+  define_singleton_method(:clear) do
+    @@vehicles = []
   end
 end
--- spec/car_dealership_spec.rb
+++ spec/car_dealership_spec.rb
@@ -2,6 +2,10 @@ require('rspec')
 require('car_dealership')
 
 describe(Vehicle) do
+  before() do
+    Vehicle.clear()
+  end
+  
   describe('#make') do
     it('return the make of the vehicle') do
       test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
@@ -9,5 +13,25 @@ describe(Vehicle) do
       expect(test_vehicle.make()).to(eq("Toyota"))
     end
   end
-  
+  describe("#model") do
+    it("return the model of the vehicle") do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_vehicle.save()
+      expect(test_vehicle.model()).to(eq("Prius"))
+    end
+  end
+  describe("#year") do
+    it("returns the year of the vehicle") do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_vehicle.save()
+      expect(test_vehicle.year()).to(eq(2000))
+    end
+  end
+  describe("#save") do
+    it("adds a vehicle to the array of saved vehicles") do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_vehicle.save()
+      expect(Vehicle.all()).to(eq([test_vehicle]))
+    end
+  end
 end
